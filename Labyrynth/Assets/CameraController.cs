167702c355f76167226db72f79c87d7c04bde362
ender-titan1 <supszym02@gmail.com>
2022-03-04 16:55:51 +0100

    Add controllers

---
+++ Labyrynth/Assets/CameraController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float mouseSensivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
+    }
+
+    void Update()
+    {
+        RortateCam();
+    }
+    void RortateCam()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
